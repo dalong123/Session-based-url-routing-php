265ed927cf30502ac42ad3ba7aa83bdbe289b29b
rukverc <kerekes.zoltan@gmail.com>
2014-08-12 08:54:48 +0200

    small update

--- inc/main_welcome.php
+++ inc/main_welcome.php
@@ -1,3 +1,5 @@
 <?php echo "This is the included welcome php"; ?>
 <br>
 You can extend the variables with id and action, which is coded already in the script bottom of the index.php.
+
+The example below is equivalent with : http://localhost/index.php?included=welcome&id=8&action=edit
