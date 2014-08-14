6c5dc09ef0ac50fe00947cdbcfed7f44f8fad73f
rukverc <kerekes.zoltan@gmail.com>
2014-08-14 23:34:21 +0200

    added some help message to class based solution

--- classes/test_class.php
+++ classes/test_class.php
@@ -3,5 +3,11 @@ class test {
  
 public function hello($variable){
 	echo "Hello Tester! u have got $variable email message(s)! "; 
+	echo "<br>";
+	echo "The generated code for the the aoutoload : <code>".htmlentities('  <?php $_class = new test();>')."</code>";
+	echo "<br>";
+	echo "The generated code for the include : <code>".htmlentities('  <?php $_class->hello(8);>')."</code>";
  }
+
+ 
 }
